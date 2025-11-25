dab7d4cb6d1ba86e806b9f167d6cf4861af0de3f
Shivam Gupta <shivamgupta86923@gmail.com>
2025-11-25 23:16:03 +0530

    Fix user not found response handling
    Refactor user not found response to use return statement.

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -63,11 +63,11 @@ export const userLogin = async (req,res)=>{
         const {email,password} = req.body;
         const user = await User.findOne({email});
         if(!user){
-            res.status(404).json({message: "User not found"})
+            return res.status(404).json({message: "User not found"})
         }
         const isMatch = await bcrypt.compare(password,user.password)
         if(!isMatch){
-            res.status(404).json({message: "User not found"})
+            return res.status(404).json({message: "User not found"})
         }
         createTokenSaveCookie(user._id,res);
         return res.status(201).json({
