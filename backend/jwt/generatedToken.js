3d1cf553285b3a8dcfe6055666e892b26c5daaf5
Shivam Gupta <shivamgupta86923@gmail.com>
2025-11-25 20:48:27 +0530

    Update generatedToken.js

--- backend/jwt/generatedToken.js
+++ backend/jwt/generatedToken.js
@@ -1,12 +1,17 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-export const createTokenSaveCookie = (userId,res ) =>{
-    const token = jwt.sign({userId}, process.env.JWT_TOKEN, {
+export const createTokenSaveCookie = (userId, res) => {
+    const token = jwt.sign({ userId }, process.env.JWT_TOKEN, {
         expiresIn: "5d",
     });
+
     res.cookie("jwt", token, {
         httpOnly: true,
         secure: true,
-        sameSite: "strict",
-    })
-}
+        sameSite: "none",
+        path: "/",
+        maxAge: 5 * 24 * 60 * 60 * 1000 // 5 days
+    });
+
+    return token;
+};
