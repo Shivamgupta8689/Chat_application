8f1d577c22e21c4ec94ef2956fff42be8a7fbdd3
Shivam Gupta <shivamgupta86923@gmail.com>
2025-11-25 23:21:02 +0530

    Update GetAllUser.jsx

--- frontend/src/context/GetAllUser.jsx
+++ frontend/src/context/GetAllUser.jsx
@@ -1,31 +1,27 @@
 import React, { useEffect, useState } from 'react'
-import Cookies from "js-cookie"
-import axios from 'axios'
 import API from '../utils/axiosInstance'
 
 const GetAllUser = () => {
     const [allUsers, setAllUser] = useState([])
-    const [loading, setLoading] = useState([])
-    useEffect(()=>{
-        const getUser = async ()=>{
-            setLoading(true);
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        const getUser = async () => {
+            setLoading(true)
             try {
-                const token = Cookies.get("jwt")
-                const response = await API.get("/api/user/getUserProfile", {
-                    withCredentials: true,
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    }
-                })
+                const response = await API.get("/api/user/getUserProfile");
+                
                 setAllUser(response.data.filteredUser)
                 setLoading(false)
             } catch (error) {
-                console.log("Error in GetAllUser" + error)
+                console.log("Error in GetAllUser", error)
+                setLoading(false)
             }
         }
         getUser()
     }, [])
-    return [allUsers , loading]
+
+    return [allUsers, loading]
 }
 
 export default GetAllUser
